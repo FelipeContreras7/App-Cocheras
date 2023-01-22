17ce213fa12bcc59ff847b320ccb6ec1e8a42654
ExpoJuy <crystaldesarrollo@gmail.com>
2023-01-22 13:12:05 -0300

    Tercer commit, conexion realizada con firebase y carga de vehículos por hora terminada

--- src/components/SignInForm/SignInForm.jsx
+++ src/components/SignInForm/SignInForm.jsx
@@ -1,16 +1,68 @@
 import React from "react";
 import "./signinform.css";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 import { createNewHourVehicle } from "../../services/firestore";
 
 function SignInForm() {
+  const navigate = useNavigate();
+
+  const [dataForm, setDataForm] = useState({
+    patente: "",
+    tipovehiculo: "",
+  });
+
+  function handleSelect(e) {
+    let name = e.target.name;
+    const valor = e.target.value;
+
+    const newDataForm = { ...dataForm };
+    newDataForm[name] = valor;
+    setDataForm(newDataForm);
+  }
+
+  function inputChangeHandler(evt) {
+    let name = evt.target.name;
+    let value = evt.target.value.toUpperCase();
+
+    const newDataForm = { ...dataForm };
+    newDataForm[name] = value;
+    setDataForm(newDataForm);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const datosporhora = {
+      patente: dataForm["patente"],
+      tipo: dataForm["tipovehiculo"],
+      fechaentrada: new Date(),
+      fechasalida: null,
+    };
+
+    console.log(dataForm);
+
+    createNewHourVehicle(datosporhora).then(navigate(`/`));
+
+    alert("El vehículo se cargo correctamente");
+  }
+
   return (
-    <form className="formularioCarga" action="">
-      <input type="text" name="patente" placeholder="PATENTE" required />
-      <select name="vehiculo" id="vehículo" required>
-        <option value="volvo">AUTO</option>
-        <option value="saab">CAMIONETA</option>
-        <option value="mercedes">MOTO</option>
+    <form className="formularioCarga" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="patente"
+        placeholder="PATENTE"
+        onChange={inputChangeHandler}
+        required
+      />
+      <select name="tipovehiculo" onChange={inputChangeHandler} required>
+        <option value="" disabled selected>
+          Seleccione un tipo
+        </option>
+        <option value="AUTO">AUTO</option>
+        <option value="CAMIONETA">CAMIONETA</option>
+        <option value="MOTO">MOTO</option>
       </select>
       <button className="btnConfirmar" type="submit">
         Cargar vehículo
